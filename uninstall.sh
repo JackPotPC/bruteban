aca6eb8119b31878aff4c9389abd854f71b252dc
JackpotPC <msharapovkarim@gmail.com>
2026-04-07 00:09:53 +0300

    Fixed uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -2,18 +2,39 @@
 set -e
 
 APP_NAME="bruteban"
-INSTALL_DIR="/opt/$APP_NAME"
+INSTALL_DIR="/usr/local/lib/$APP_NAME"
+CONFIG_DIR="/etc/$APP_NAME"
+LOG_DIR="/var/log/$APP_NAME"
+SERVICE_FILE="$APP_NAME.service"
 
-if [[ $EUID -ne 0 ]]; then
-  echo "Run as root"
-  exit 1
-fi
+check_root() {
+  if [[ $EUID -ne 0 ]]; then
+    echo "Запустить с sudo"
+    exit 1
+  fi
+}
 
-systemctl stop $APP_NAME || true
-systemctl disable $APP_NAME || true
-rm -f /etc/systemd/system/$APP_NAME.service
-systemctl daemon-reload
+uninstall_app() {
+  rm -rf $INSTALL_DIR
+  rm -rf $CONFIG_DIR
+  rm -rf $LOG_DIR
+}
 
-rm -rf "$INSTALL_DIR"
+stop_service() {
+  systemctl stop $APP_NAME || true
+  systemctl disable $APP_NAME || true
+  rm -f /etc/systemd/system/$SERVICE_FILE
+  systemctl daemon-reload
+}
 
-echo "[✓] $APP_NAME removed"
+main() {
+  echo "[*] Проверка root..."
+  check_root
+  echo "[*] Остановка сервиса..."
+  stop_service
+  echo "[*] Удаление приложения..."
+  uninstall_app
+  echo "[*] Удаление завершено."
+}
+
+main
